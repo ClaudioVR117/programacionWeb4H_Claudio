bcd794f6dbf0edcad17de80595300439ac938b64
ClaudioVR117 <cvazquez45@ucol.mx>
2026-04-26 14:33:42 -0600

    Actividad 16 Registro de estudiantes

---
+++ BlazorApp1/Models/Estudiante.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Estudiante
+    {
+        public string Nombre { get; set; } = "";
+        public string Correo { get; set; } = "";
+        public double Calificacion { get; set; }
+    }
+}
